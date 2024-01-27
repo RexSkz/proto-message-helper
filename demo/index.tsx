a02bc1d3580139f2edec18f659bb389f9725f822
Rex Zeng <rex@rexskz.info>
2024-01-27 13:38:04 +0800

    docs: fix fork-me-on-github icon

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Buffer } from 'buffer/';
+import ForkMeOnGithub from 'fork-me-on-github';
 
 import { decoder, Viewer } from '../src';
 import type { DecodeResult } from '../src';
@@ -141,13 +142,7 @@ const Demo: React.FC = () => {
       <div className="demo-footer">
         <p>Made with ♥ by Rex Zeng</p>
       </div>
-      <a href="https://github.com/rexskz/proto-message-helper">
-        <img
-          style={{ position: 'absolute', top: 0, right: 0, border: 'none' }}
-          src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
-          alt="Fork me on GitHub"
-        />
-      </a>
+      <ForkMeOnGithub.default repo="https://github.com/rexskz/proto-message-helper" />
     </div>
   );
 };
